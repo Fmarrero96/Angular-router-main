c38725eb65797b87dbc13b841af17beb1a097c81
Facundo Marrero <facu_marrero@hotmail.com>
2022-09-03 23:01:04 -0300

    creando primeras rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryComponent } from './pages/category/category.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'category',
+    component: CategoryComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
