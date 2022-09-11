bebc9a4bdadd6c23b7826f7f6410b36146d7e3f1
Facundo Marrero <facu_marrero@hotmail.com>
2022-09-11 20:47:38 -0300

    paginas anidadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,39 @@
 import { NgModule } from '@angular/core';
+import { LayoutComponent } from './website/components/layout/layout.component';
 import { RouterModule, Routes } from '@angular/router';
-import { CategoryComponent } from './pages/category/category.component';
-import { HomeComponent } from './pages/home/home.component';
-import { MycartComponent } from './pages/mycart/mycart.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { CategoryComponent } from './website/pages/category/category.component';
+import { HomeComponent } from './website/pages/home/home.component';
+import { MycartComponent } from './website/pages/mycart/mycart.component';
+import { NotFoundComponent } from './website/pages/not-found/not-found.component';
+import { ProductDetailComponent } from './website/pages/product-detail/product-detail.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    component: HomeComponent
-  },
-  {
-    path: 'category/:id',
-    component: CategoryComponent
-  },
-  {
-    path: 'my-cart',
-    component: MycartComponent
-  },
-  {
-    path: 'product/:id',
-    component: ProductDetailComponent
+    component: LayoutComponent,
+    children:[
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'category/:id',
+        component: CategoryComponent
+      },
+      {
+        path: 'my-cart',
+        component: MycartComponent
+      },
+      {
+        path: 'product/:id',
+        component: ProductDetailComponent
+      }
+    ]
   },
   {
     path: '**', //cuando no encuentra que exista el path te manda por defecto con **
