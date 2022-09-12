25ee74a907ce31b94fbf108ec09e192fb958fdc4
Facundo Marrero <facu_marrero@hotmail.com>
2022-09-11 23:19:12 -0300

    creando website module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,39 +1,12 @@
 import { NgModule } from '@angular/core';
-import { LayoutComponent } from './website/components/layout/layout.component';
 import { RouterModule, Routes } from '@angular/router';
-import { CategoryComponent } from './website/pages/category/category.component';
-import { HomeComponent } from './website/pages/home/home.component';
-import { MycartComponent } from './website/pages/mycart/mycart.component';
-import { NotFoundComponent } from './website/pages/not-found/not-found.component';
-import { ProductDetailComponent } from './website/pages/product-detail/product-detail.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
 
 const routes: Routes = [
   {
     path: '',
-    component: LayoutComponent,
-    children:[
-      {
-        path: '',
-        redirectTo: '/home',
-        pathMatch: 'full'
-      },
-      {
-        path: 'home',
-        component: HomeComponent
-      },
-      {
-        path: 'category/:id',
-        component: CategoryComponent
-      },
-      {
-        path: 'my-cart',
-        component: MycartComponent
-      },
-      {
-        path: 'product/:id',
-        component: ProductDetailComponent
-      }
-    ]
+    loadChildren: () => import('./website/website.module').then(m => m.WebsiteModule)
   },
   {
     path: 'cms',
---
+++ src/app/website/website-routing.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './components/layout/layout.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { MycartComponent } from './pages/mycart/mycart.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children:[
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'category/:id',
+        component: CategoryComponent
+      },
+      {
+        path: 'my-cart',
+        component: MycartComponent
+      },
+      {
+        path: 'product/:id',
+        component: ProductDetailComponent
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class WebsiteRoutingModule { }
